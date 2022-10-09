000e2cbd37a9418bcd5256011c368ef5e4b01e5a
Krygore <webdev.krygore@gmail.com>
2022-10-08 19:28:41 -0700

    completed guided project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Playground from './components/Playground'
 
 /*
 💥💥💥 Rules when DECLARING a React component 💥💥💥
@@ -25,11 +26,13 @@ function App(props) {
   return (
     <div className='container'>
       <h1>Welcome to React, {props.track} rockstars!!</h1>
+      <h2>Student name: {props.name}, age: {props.age}</h2>
+      <Playground />
     </div>
   )
 }
 
 render(
-  <App track='Web' />,
+  <App track='Web' name='Michael' age='28' />,
   document.querySelector('#root')
 )
